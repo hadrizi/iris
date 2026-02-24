4174d2665ca8eb742b9b81161d34f27dc09baaf7
hadrizi <vbyhgn@gmail.com>
2026-02-24 16:58:25 +0100

    fix: ARGB color format instead of RGBA

--- src/graphics/graphics.hpp
+++ src/graphics/graphics.hpp
@@ -16,19 +16,19 @@ namespace iris {
 
 typedef uint32_t pixel_t;
 
-#define PIXEL_COL(r, g, b, a) ((uint32_t)(((uint8_t)r << 24) | ((uint8_t)g << 16) | ((uint8_t)b << 8) | (uint8_t)a))
-#define PIXEL_RED(p)   ((uint8_t)((uint32_t)p >> 24))
-#define PIXEL_GREEN(p) ((uint8_t)((uint32_t)p >> 16))
-#define PIXEL_BLUE(p)  ((uint8_t)((uint32_t)p >> 8))
-#define PIXEL_ALPHA(p) ((uint8_t)((uint32_t)p))
+#define PIXEL_COL(r, g, b, a) ((uint32_t)(((uint8_t)a << 24) | ((uint8_t)r << 16) | ((uint8_t)g << 8) | (uint8_t)b))
+#define PIXEL_RED(p)   ((uint8_t)((uint32_t)p >> 16))
+#define PIXEL_GREEN(p) ((uint8_t)((uint32_t)p >> 8))
+#define PIXEL_BLUE(p)  ((uint8_t)((uint32_t)p))
+#define PIXEL_ALPHA(p) ((uint8_t)((uint32_t)p >> 24))
 
 // Helper color consts
 
-constexpr pixel_t red    = 0xFF000000;
-constexpr pixel_t green  = 0x00FF0000;
-constexpr pixel_t blue   = 0x0000FF00;
+constexpr pixel_t red    = 0x00FF0000;
+constexpr pixel_t green  = 0x0000FF00;
+constexpr pixel_t blue   = 0x000000FF;
 constexpr pixel_t white  = 0xFFFFFFFF;
-constexpr pixel_t black  = 0x00000000;
+constexpr pixel_t black  = 0xFF000000;
 
 // ==================
 
